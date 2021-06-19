7e9def44c149cf70eb8d8d0cb0d83bf705ead5a5
lau <twigliu@hotmail.com>
2021-06-19 23:31:18 +0800

    Promise目录

---
+++ ES6/11-Promise与异步编程.js
@@ -0,0 +1,26 @@
+/**
+ * 1.异步编程的背景
+ *   1.1事件模型
+ *   1.2回调模式
+ * 2.Promise基础
+ *   2.1Promise的声明周期
+ *   2.2创建未决的Promise
+ *   2.3创建已决的Promise
+ *      2.3.1 使用Promise.resolve()
+ *      2.3.2 使用Promise.reject()
+ *      2.3.3 非Promise 的 Thenable
+ *   2.4执行器错误
+ * 3.全局的Promise拒绝处理
+ *   3.1Node.js的拒绝处理
+ *   3.2浏览器的拒绝处理
+ * 4.串联Promise
+ *   4.1捕获错误
+ *   4.2在Promise链中返回值
+ *   4.3在Promise链中返回Promise
+ * 5.响应多个Promise
+ *   5.1Promise.all()方法
+ *   5.2Promise.race()方法
+ * 6.继承Promise
+ * 7.异步任务运行
+ * 8.总结
+ */
