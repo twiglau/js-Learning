5233d9943fb489ec4269c157eb1fef433be1e434
lau <twigliu@hotmail.com>
2021-11-06 18:04:03 +0800

    依赖模块

---
+++ 忍者秘籍/11-模块(未看)/02-AMD和CommonJS.js
@@ -0,0 +1,10 @@
+/**
+ * AMD和CommonJS是两个相互竞争的标准,均可以定义JavaScript模块. 除了语法和原理的区别之外,
+ * 主要的区别是AMD的设计理念是明确基于浏览器, 而CommonJS的设计是面向通过JavaScript环境
+ * (如Node.js服务端),而不局限于浏览器.
+ */
+
+/**
+ * 1. AMD
+ * AMD最流行的实现是 RequireJS
+ */
