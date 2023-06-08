c27697b5fe61efb380b40d5bbd4fad4a1ab00a5e
15729080020 <twigliu@163.com>
2023-06-08 18:20:00 +0800

    修改

---
+++ 基础/15_ES6中类的使用/01_class定义类的方式.js
@@ -0,0 +1,19 @@
+// 类的声明
+class Person {
+
+}
+
+// 类的表达式
+// function foo() {
+
+// }
+
+// 研究一下类的特点
+console.log(Person.prototype)
+console.log(Person.prototype.__proto__)
+console.log(Person.prototype.constructor)
+console.log(typeof Person)
+
+var p = new Person()
+console.log(p.__proto__)
+console.log(p.__proto__ === Person.prototype)
---
+++ 基础/15_ES6中类的使用/02_class的构造方法.js
@@ -0,0 +1,9 @@
+
+
+// 类的声明
+class Person {
+    // 类的构造方法
+    constructor(name, age) {
+
+    }
+}
