d86fc3b43ab5911907254680c4ed563466a7d94e
twiglauer <twigliu@163.com>
2023-07-08 18:18:34 +0800

    修改

--- 模块化/02_exports/lau.js
+++ 模块化/02_exports/lau.js
@@ -5,10 +5,6 @@ function sum(num1, num2) {
     return num1 * num2
 }
 
-// 第二种导出方式
-exports.name = a_name
-exports.age = age
-exports.sum = sum
 
 
 // module.exports 和 exports 有什么区别?
@@ -16,3 +12,16 @@ exports.sum = sum
 // 源码是如何处理的?
 // 1. module.exports = {}
 // 2. exports = module.exports
+
+// 第二种导出方式, 相当于往 module.exports 对象上添加属性
+exports.name = a_name
+exports.age = age
+exports.sum = sum
+
+// 第三种 是不对的, 最终导出的是一定是 module.exports = {} 
+// 只有 module.exports 对象里面有属性了, exports 里面才有属性对象
+// 以下写法, module.exports 对象并没有三种属性
+// exports = {
+//     name,age,sum
+// }
+
---
+++ 模块化/02_require细节/main.js
@@ -0,0 +1,8 @@
+
+// 1. 核心模块 : 直接返回
+const path = require('path')
+path.resolve()
+
+// 2. 情况二: 路径  ./ ../   /(根目录)
+
+const model = require('./module')
